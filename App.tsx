d7bdef6dc2b578249817a1d9cd8fef4d0b9be717
Manuel Garcia <manueldev250799@gmail.com>
2024-02-02 03:28:49 -0500

    chore: improve animations

--- App.tsx
+++ App.tsx
@@ -4,13 +4,7 @@ import Animated, {
   useSharedValue,
   withSpring,
 } from 'react-native-reanimated';
-import {
-  Dimensions,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  View,
-} from 'react-native';
+import {StatusBar, View, useWindowDimensions} from 'react-native';
 
 import {
   Gesture,
@@ -22,14 +16,14 @@ import {
 //   'https://images.pexels.com/photos/18175115/pexels-photo-18175115/free-photo-of-ojos.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
 
 const secondImageURI =
-  'https://images.pexels.com/photos/10859633/pexels-photo-10859633.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
-
-const {width, height} = Dimensions.get('window');
+  'https://e0.pxfuel.com/wallpapers/705/509/desktop-wallpaper-city-pop-aesthetic-skyline.jpg';
 
 function App(): React.JSX.Element {
   const scale = useSharedValue(1);
   const focalX = useSharedValue(0);
   const focalY = useSharedValue(0);
+
+  const {width, height} = useWindowDimensions();
 
   const savedFocalX = useSharedValue(0);
   const savedFocalY = useSharedValue(0);
@@ -40,10 +34,11 @@ function App(): React.JSX.Element {
       scale.value = clamp(event.scale * savedScale.value, 1, 3);
       focalX.value = event.focalX;
       focalY.value = event.focalY;
-    })
-    .onEnd(() => {
+
       savedFocalX.value = focalX.value;
       savedFocalY.value = focalY.value;
+    })
+    .onEnd(() => {
       savedScale.value = clamp(scale.value, 1, 3);
     });
 
@@ -54,21 +49,8 @@ function App(): React.JSX.Element {
       focalY.value = savedFocalY.value + -event.translationY / scale.value;
     })
     .onEnd(() => {
-      if (focalX.value >= width) {
-        focalX.value = width;
-      }
-      if (focalX.value <= 0) {
-        focalX.value = 0;
-      }
-
-      if (focalY.value >= height) {
-        focalY.value = height;
-      }
-
-      if (focalY.value <= 0) {
-        focalY.value = 0;
-      }
-
+      focalX.value = clamp(focalX.value, 0, width);
+      focalY.value = clamp(focalY.value, 0, height);
       savedFocalX.value = focalX.value;
       savedFocalY.value = focalY.value;
     });
@@ -97,44 +79,46 @@ function App(): React.JSX.Element {
   const animatedStyle = useAnimatedStyle(() => {
     return {
       transform: [
-        {translateX: withSpring(focalX.value, {duration: 250})},
-        {translateY: withSpring(focalY.value, {duration: 250})},
-        {translateX: withSpring(-width / 2, {duration: 250})},
-        {translateY: withSpring(-height / 2, {duration: 250})},
-        {scale: withSpring(scale.value, {duration: 250})},
-        {translateX: withSpring(-focalX.value, {duration: 250})},
-        {translateY: withSpring(-focalY.value, {duration: 250})},
-        {translateX: withSpring(width / 2, {duration: 250})},
-        {translateY: withSpring(height / 2, {duration: 250})},
+        {translateX: withSpring(focalX.value, {duration: 400})},
+        {translateY: withSpring(focalY.value, {duration: 400})},
+        {translateX: withSpring(-width / 2, {duration: 400})},
+        {translateY: withSpring(-height / 2, {duration: 400})},
+        // scale smoothly
+        {scale: withSpring(scale.value, {duration: 400})},
+        {translateX: withSpring(-focalX.value, {duration: 400})},
+        {translateY: withSpring(-focalY.value, {duration: 400})},
+        {translateX: withSpring(width / 2, {duration: 400})},
+        {translateY: withSpring(height / 2, {duration: 400})},
       ],
     };
   });
 
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <StatusBar barStyle="dark-content" />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        contentContainerStyle={{flex: 1, flexGrow: 1}}>
-        <GestureHandlerRootView style={{flex: 1}}>
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: '#080808',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <GestureDetector
-              gesture={Gesture.Race(pinchGesture, panGesture, tapGesture)}>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#000" />
+      <GestureHandlerRootView style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: '#080808'}}>
+          <GestureDetector
+            gesture={Gesture.Race(pinchGesture, panGesture, tapGesture)}>
+            <Animated.View
+              style={[
+                {
+                  flex: 1,
+                  backgroundColor: '#080808',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                },
+                animatedStyle,
+              ]}>
               <Animated.Image
-                style={[{width, height, objectFit: 'cover'}, animatedStyle]}
+                style={[{width, height, objectFit: 'cover'}]}
                 source={{uri: secondImageURI}}
               />
-            </GestureDetector>
-          </View>
-        </GestureHandlerRootView>
-      </ScrollView>
-    </SafeAreaView>
+            </Animated.View>
+          </GestureDetector>
+        </View>
+      </GestureHandlerRootView>
+    </>
   );
 }
 
